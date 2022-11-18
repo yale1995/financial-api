0227f7084d6517b774e268ebbf31d4bc24a11eff
Yale Araujo <yale850@gmail.com>
2022-11-18 16:53:53 -0300

    feat: creating a middleware verifying account

--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf == cpf);
+
+  if (!customer) {
+    return response
+      .status(400)
+      .json({ status: "error", message: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -27,15 +43,8 @@ app.post("/account", (request, response) => {
   return response.status(201).json({ status: "success" });
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find((customer) => customer.cpf == cpf);
-
-  if(!customer) {
-    return response.status(400).json({status: 'error', message: 'Customer not found'})
-  }
-
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
 
